2f18dce737e2c07e0472eb8e5f36703254304703
Drew Raines <aaraines@gmail.com>
2011-02-09 15:25:13 -0600

    Move to ring-0.3.5.

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [enlive "1.0.0-SNAPSHOT"]
                  [compojure "0.4.1"]
                  [hiccup "0.2.6"]
-                 [ring/ring "0.2.5"]
+                 [ring/ring "0.3.5"]
                  [org.markdownj/markdownj "0.3.0-1.0.2b4"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
                      [lein-release "1.1.1"]
