bba6183e7d882d5cb527372719ad1c65ed0c5a9b
Drew Raines <aaraines@gmail.com>
2011-08-18 14:39:05 -0500

    Add paste store to servlet.

--- src/elmer/servlet.clj
+++ src/elmer/servlet.clj
@@ -1,8 +1,9 @@
 (ns elmer.servlet
   (:use [ring.util.servlet :only [defservice]]
         [elmer.core :only [app]]
-        [elmer.context :only [wrap-context-path]])
+        [elmer.context :only [wrap-context-path]]
+        [elmer.middleware :only [wrap-paste-store]])
   (:gen-class
    :extends javax.servlet.http.HttpServlet))
 
-(defservice (wrap-context-path app))
+(defservice (-> app wrap-context-path wrap-paste-store))
