98fe0e49a142a21304836a80cf63dd43af797ffa
Drew Raines <aaraines@gmail.com>
2012-10-23 17:02:19 -0500

    Finish S3 authorization, PUT.

--- src/elmer/store/s3.clj
+++ src/elmer/store/s3.clj
@@ -7,33 +7,66 @@
   `(let [~store (s3-store ~conf)]
      ~@body))
 
-(defn get* [loc pre name]
-  (log/debug 'get* name)
-  (:content
-   (s3/get-object (:creds loc) (:bucket loc) (format "%s/%s" pre name))))
+(defn abs-path [loc root name]
+  (format "%s/%s/%s" (:bucket loc) root name))
 
-(defn put* [loc pre name keypre key is]
-  (log/debug 'put* (:bucket loc) (format "%s/%s" pre name))
+(defn key-path [key-root name]
+  (format "%s/%s.key" key-root name))
+
+(defn store-key [loc key-root name key]
   (s3/put-object (:creds loc)
                  (:bucket loc)
-                 (format "%s/%s.key" keypre name)
+                 (key-path key-root name)
                  key
-                 {:content-type "text/plain"})
-  (s3/put-object (:creds loc) (:bucket loc) (format "%s/%s" pre name) is))
+                 {:content-type "text/plain"}))
 
-(defn authorized?* []
-  )
+(defn get-key [loc key-root name]
+  (slurp
+   (:content (s3/get-object (:creds loc) (:bucket loc)
+                            (key-path key-root name)))))
 
-(deftype S3Store [loc pastes keys]
+(defn key-exists? [loc key-root name]
+  (s3/object-exists? (:creds loc) (:bucket loc)
+                     (key-path key-root name)))
+
+(defn key-valid? [loc key-root name key]
+  (= key (get-key loc key-root name)))
+
+(defn authorized?* [loc root key-root name key]
+  (let [yes? (if (key-exists? loc key-root name)
+               (key-valid? loc key-root name key)
+               true)]
+    (log/debug "auth" (key-path key-root name) "->" (if yes? "YES" "NO"))
+    yes?))
+
+(defn get* [loc pre name]
+  (log/debug 'get* name)
+  (slurp
+   (:content
+    (s3/get-object (:creds loc) (:bucket loc) (format "%s/%s" pre name)))))
+
+(defn put* [loc root key-root name key is]
+  (let [f (format "%s/%s" root name)]
+    (when (authorized?* loc root key-root name key)
+      (store-key loc key-root name key)
+      (log/debug "store" (abs-path loc root name))
+      (s3/put-object (:creds loc) (:bucket loc)
+                     (format "%s/%s" root name) is))))
+
+(deftype S3Store [loc root key-root]
   PasteStore
   (get [_ name]
-    (get* loc pastes name))
+    (get* loc root name))
   (put [_ name key is]
-    (put* loc pastes name keys key is))
+    (put* loc root key-root name key is))
   (authorized? [_ name key]
     (authorized?*)))
 
 (defrecord S3Location [creds bucket])
+
+(defn make-s3loc [m]
+  (S3Location. (select-keys m [:access-key :secret-key])
+               (:bucket m)))
 
 (defn s3-store [{:keys [access-key secret-key
                         bucket key-pastes key-keys]
@@ -42,6 +75,4 @@
              "bucket" bucket
              "pastes" key-pastes
              "keys" key-keys)
-  (S3Store.
-   (S3Location. (select-keys opts [:access-key :secret-key]) bucket)
-   key-pastes key-keys))
+  (S3Store. (make-s3loc opts) key-pastes key-keys))
--- test/elmer/test/store/s3.clj
+++ test/elmer/test/store/s3.clj
@@ -4,11 +4,32 @@
   (:use [clojure.test]
         [clojure.java.io :as io]))
 
-(deftest t-paste
-  (testing "should put a paste"
-    (with-s3-store [store (-> (str (System/getProperty "user.home")
-                                   "/.elmer.clj")
-                              slurp read-string)]
-      (is (store/put store "foo.txt" "sekrat" "bytes"))
-      (is (= "bytes" (slurp (store/get store "foo.txt"))))))
-  )
+(defn creds []
+  (-> (str (System/getProperty "user.home")
+           "/.elmer.clj")
+      slurp read-string))
+
+(defn make-paste-name []
+  (format "test/elmer-%s.txt" (subs (str (java.util.UUID/randomUUID)) 0 6)))
+
+(deftest ^{:integration true}
+  t-paste
+  (let [f (make-paste-name)]
+    (testing "should put a paste"
+      (with-s3-store [store (creds)]
+        (is (store/put store f "sekrat" "bytes"))
+        (is (= "bytes" (store/get store f)))))
+    (testing "should put a paste from an InputStream"
+      (with-s3-store [store (creds)]
+        (is (store/put store f "sekrat" (java.io.ByteArrayInputStream.
+                                         (.getBytes "bytes"))))
+        (is (= "bytes" (store/get store f)))))))
+
+(deftest ^{:integration true}
+  t-update
+  (let [f (make-paste-name)]
+    (testing "should update a paste, or not if key invalid"
+      (with-s3-store [store (creds)]
+        (is (store/put store f "sekrat" "bytes"))
+        (is (store/put store f "sekrat" "bytes"))
+        (is (not (store/put store f "bad" "bytes")))))))
